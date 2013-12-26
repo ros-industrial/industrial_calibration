d4dbc440e88abb2c25b87c5907e8a48dd1c8717e
Christina Gomez <christina.gomez@swri.org>
2013-12-26 13:47:18 -0600

    Deleted some pramenters/functions that were not used/useful. Finished doxygen comments and re-formatted others. Incorporated new pattern parameters.

--- industrial_extrinsic_cal/include/industrial_extrinsic_cal/ros_camera_observer.h
+++ industrial_extrinsic_cal/include/industrial_extrinsic_cal/ros_camera_observer.h
@@ -28,83 +28,81 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/CameraInfo.h>
-#include <tf/transform_listener.h>
-#include <tf/transform_broadcaster.h>
 #include <geometry_msgs/PointStamped.h>
 
 namespace industrial_extrinsic_cal {
 
-	/**
-	 * @brief enumerator containing three options for the type of pattern to detect
-	 */
-    enum pattern_options_
-    {
-    	Chessboard, CircleGrid, AsymmetricCircleGrid
-    };
- class ROSCameraObserver: public CameraObserver
- {
-  public:
 
-    /** @brief constructor */
-    /** @param source_name name of image topic */
-    ROSCameraObserver(std::string image_topic="");
+class ROSCameraObserver: public CameraObserver
+{
+public:
 
-    /** @brief Default destructor */
-    ~ROSCameraObserver(){};
+	/** @brief constructor */
+	/** @param source_name name of image topic */
+	ROSCameraObserver(std::string image_topic="");
 
-    /** @brief add a target to look for */
-    /** @param targ a target to look for */
-    /** @param roi Region of interest for target */
-    void addTarget(boost::shared_ptr<Target> targ, Roi roi);
+	/** @brief Default destructor */
+	~ROSCameraObserver(){};
 
-    /** @brief remove all targets */
-    void clearTargets();
+	/** @brief add a target to look for and regoing to look in */
+	/** @param targ a target to look for */
+	/** @param roi Region of interest for target */
+	void addTarget(boost::shared_ptr<Target> targ, Roi roi);
 
-    /** @brief clear all previous observations */
-    void clearObservations();
+	/** @brief remove all targets */
+	void clearTargets();
 
-    /** @brief return observations */
-    /** @param output all observations of targets defined */
-    int getObservations(CameraObservations &camera_observations);
+	/** @brief clear all previous observations */
+	void clearObservations();
+
+	/** @brief return observations */
+	/** @param output all observations of targets defined */
+	int getObservations(CameraObservations &camera_observations);
 
 
-  private:
+private:
 
-    //ROS specific functions
-    /**
-     * @brief
-     * @param image_msg
-     */
-    void infoCallback(const sensor_msgs::ImageConstPtr& image_msg);
+	/** @brief enumerator containing three options for the type of pattern to detect */
+	enum pattern_options_
+	{
+		Chessboard=0, CircleGrid=1, ARtag=2
+	}pattern_;
 
-    /**
-     *  @brief pattern to be used in detection
-     */
-    pattern_options_ pattern_;
-    /**
-     *  @brief topic name for image which is input at constructor
-     */
-    std::string image_topic_;
-    /**
-     *  @brief cropped image based on original image and region of interest
-     */
-    cv::Mat image_roi_;
-    std::vector<cv::Point2f> observation_pts_;
-    boost::shared_ptr<Target> instance_target_;
+	/**  @brief topic name for image which is input at constructor */
+	std::string image_topic_;
+	/** @brief cropped image based on original image and region of interest */
+	cv::Mat image_roi_;
+	/** @brief target pattern grid number of rows	 */
+	int pattern_rows_;
+	/** @brief target pattern grid number of columns	 */
+	int pattern_cols_;
+	/** @brief circle grid target pattern true=symmetric	 */
+	bool sym_circle_;
+	/** @brief 2D values of corner/circle locations returned from cv methods */
+	std::vector<cv::Point2f> observation_pts_;
+	/** @brief private target which is initialized to input target */
+	boost::shared_ptr<Target> instance_target_;
+	/** @brief private CameraObservations which are set at the end of getObservations and cleared */
+	CameraObservations camera_obs_;
 
-    //ROS specific params
-    ros::NodeHandle nh_;
-    ros::Subscriber image_sub_;
-    ros::Publisher results_pub_;
-	tf::TransformListener tf_listener_;
-	tf::TransformBroadcaster tf_broadcaster_;
+	//ROS specific params
+	/** @brief ROS node handle for initializing point cloud publisher */
+	ros::NodeHandle nh_;
+	/** @brief ROS subscriber to image_topic_ */
+	ros::Subscriber image_sub_;
+	/** @brief ROS publisher of out_bridge_ or output_bridge_ */
+	ros::Publisher results_pub_;
 
-    // Structures for interacting with ROS/CV messages
-    cv_bridge::CvImagePtr input_bridge_;
+
+	// Structures for interacting with ROS/CV messages
+	/** @brief cv_bridge image for input image from ROS topic image_topic_ */
+	cv_bridge::CvImagePtr input_bridge_;
+	/** @brief cv_bridge image for color output image */
 	cv_bridge::CvImagePtr output_bridge_;
+	/** @brief cv_bridge image for mono output image */
 	cv_bridge::CvImagePtr out_bridge_;
 
-  };
+};
 
 }//end industrial_extrinsic_cal namespace
 
