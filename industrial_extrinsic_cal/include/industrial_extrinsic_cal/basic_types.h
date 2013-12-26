b53fe5b63299a2d25392ddbeedf5accd14e0c213
Christina Gomez <christina.gomez@swri.org>
2013-12-26 13:49:22 -0600

    Added parameters for each pattern.

--- industrial_extrinsic_cal/include/industrial_extrinsic_cal/basic_types.h
+++ industrial_extrinsic_cal/include/industrial_extrinsic_cal/basic_types.h
@@ -68,11 +68,24 @@ namespace industrial_extrinsic_cal {
       };
     };
   }Pose6d;
-  
-  /* TODO create three parameter types 
-     CheckerboardParameters, CircleGridParameters, and ARTargetParameters 
-  */
-				    
+
+  /** @brief Parameters defining checker board target   */
+  typedef struct{
+	  int pattern_rows;
+	  int pattern_cols;
+  }CheckerBoardParameters;
+  /** @brief Parameters defining circle grid target  */
+  typedef struct{
+	  int pattern_rows;
+	  int pattern_cols;
+	  bool is_symmetric;
+  }CircleGridParameters;
+  /** @brief Parameters defining AR target */
+  typedef struct{
+	  //std::string marker_pattern;
+	  double marker_width;
+  }ARTargetParameters;
+
   /*! \brief A target's information */
   typedef struct{
     std::string target_name;
@@ -81,7 +94,7 @@ namespace industrial_extrinsic_cal {
       CheckerBoardParameters checker_board_parameters;
       CircleGridParameters   circle_grid_parameters;
       ARTargetParameters   ar_target_parameters;
-    }
+    };
     bool is_moving;  /**< observed in multiple locations or it fixed to ref frame */
     Pose6d pose;
     unsigned int num_points; /**< number of points in the point array */
