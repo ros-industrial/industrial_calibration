23b1630e1d5c1bddba3a2b789fe39a5d61609529
Christina Gomez <christina.gomez@swri.org>
2014-01-14 19:52:47 -0600

    Deleted all older commented out code. Added three tests.

--- industrial_extrinsic_cal/test/utest.cpp
+++ industrial_extrinsic_cal/test/utest.cpp
@@ -19,58 +19,234 @@
 #include <industrial_extrinsic_cal/calibration_job_definition.h>
 
 #include <gtest/gtest.h>
+#include <yaml-cpp/yaml.h>
+#include <fstream>
 
 using namespace industrial_extrinsic_cal;
+using industrial_extrinsic_cal::CalibrationJob;
 
-TEST(IndustrialUtilsSuite, vector_compare)
+void print_AAtoH(double x, double y, double z, double tx, double ty, double tz);
+
+std::string camera_file_name("/home/cgomez/ros/hydro/catkin_ws/src/industrial_calibration/industrial_extrinsic_cal/yaml/test1a_camera_def.yaml");
+std::string target_file_name("/home/cgomez/ros/hydro/catkin_ws/src/industrial_calibration/industrial_extrinsic_cal/yaml/test1_target_def.yaml");
+std::string caljob_file_name("/home/cgomez/ros/hydro/catkin_ws/src/industrial_calibration/industrial_extrinsic_cal/yaml/test1a_caljob_def.yaml");
+
+//CalibrationJob Cal_job(camera_file_name, target_file_name, caljob_file_name);
+CalibrationJob Cal_job(camera_file_name, target_file_name, caljob_file_name);
+
+//need: Camera intrinstics, camera extrinsics, observations (x,y),
+//camera name, target name, scene id, pnt id, target pose, and point position
+std::vector<int> point_ids;
+std::vector<float> obs_x;
+std::vector<float> obs_y;
+
+P_BLOCK intrinsics;
+P_BLOCK extrinsics;
+P_BLOCK target_pose;
+P_BLOCK pnt_pos;
+
+
+TEST(IndustrialExtrinsicCalSuite, load_check)
 {
-/*
-  std::vector<std::string> v1;
-  std::vector<std::string> v2;
-  std::vector<std::string> v3;
+  //need: Camera intrinstics, camera extrinsics, observations (x,y),
+  //camera name, target name, scene id, pnt id, target pose, and point position
 
-  // Testing empty vectors
-  EXPECT_TRUE(isSimilar(v1, v2));
-  EXPECT_TRUE(isSame(v1, v2));
+  EXPECT_TRUE(Cal_job.load());//should import camera instrinsics
 
-  std::string i1 = "item_1";
-  std::string i2 = "item_2";
-  std::string i3 = "item_3";
 
-  v1.push_back(i1);
-  v1.push_back(i2);
-  v1.push_back(i3);
+  //read in observation file which contains Points with pnt_id, x and y
+  std::ifstream obs_input_file("/home/cgomez/ros/hydro/catkin_ws/src/industrial_calibration/industrial_extrinsic_cal/observations.txt");
+  YAML::Parser obs_parser(obs_input_file);
 
-  // Testing one empty, one populated
-  EXPECT_FALSE(isSimilar(v1, v2));
-  EXPECT_FALSE(isSame(v1, v2));
+  YAML::Node obs_doc;
+  obs_parser.GetNextDocument(obs_doc);
 
-  v2.push_back(i1);
-  v2.push_back(i2);
+  // read in all observations
+  if (const YAML::Node *observations = obs_doc.FindValue("Points"))
+  {
+    ROS_DEBUG_STREAM("Found "<<observations->size() <<" observations ");
+    point_ids.resize(observations->size());
+    obs_x.resize(observations->size());
+    obs_y.resize(observations->size());
+    for (unsigned int i = 0; i < observations->size(); i++)
+    {
+      (*observations)[i]["Point_id"] >> point_ids.at(i);
 
-  // Testing one partially filled, one full
-  EXPECT_FALSE(isSimilar(v1, v2));
-  EXPECT_FALSE(isSame(v1, v2));
+      const YAML::Node *points_node = (*observations)[i].FindValue("Pnts");
+      //ROS_DEBUG_STREAM("Found "<<points_node->size() <<" points ");
 
-  v2.push_back(i3);
+      //for (int j = 0; j < points_node->size(); j++)
+      //{
+        //const YAML::Node *pnt_node = (*points_node)[j].FindValue("pnt");
+        std::vector<float> temp_pnt;
+        (*points_node) >> temp_pnt;
+        obs_x.at(i) = temp_pnt[0];
+        obs_y.at(i) = temp_pnt[1];
+        //points_node[0]>>obs_x(i);
+        //points_node[1]>>obs_y(i);
+      //}
+    }
+  }
 
-  // Testing same vectors
-  EXPECT_TRUE(isSimilar(v1, v2));
-  EXPECT_TRUE(isSame(v1, v2));
-
-  v3.push_back(i3);
-  v3.push_back(i2);
-  v3.push_back(i1);
-
-  // Testing similar but not same
-  EXPECT_TRUE(isSimilar(v1, v3));
-  EXPECT_FALSE(isSame(v1, v3));
-*/
 }
+
+TEST(IndustrialExtrinsicCalSuite, param_check)
+{
+  //need: Camera intrinstics, camera extrinsics, observations (x,y),
+  //camera name, target name, scene id, pnt id, target pose, and point position
+  std::vector<ObservationScene> obs_scene_list_ = Cal_job.getSceneList();
+
+  EXPECT_STRCASEEQ("Asus1", obs_scene_list_.at(0).cameras_in_scene_.at(0)->camera_name_.c_str());
+
+  EXPECT_STRCASEEQ("Checkerboard",
+                   obs_scene_list_.at(0).observation_command_list_.at(0).target->target_name.c_str());
+
+  CeresBlocks c_blocks_ = Cal_job.getCeresBlocks();
+  ASSERT_GT(c_blocks_.static_cameras_.size(), 0);
+
+  intrinsics = c_blocks_.getStaticCameraParameterBlockIntrinsics("Asus1");
+  extrinsics = c_blocks_.getStaticCameraParameterBlockExtrinsics("Asus1");
+
+  target_pose = c_blocks_.getStaticTargetPoseParameterBlock("Checkerboard");
+  pnt_pos = c_blocks_.getStaticTargetPointParameterBlock("Checkerboard", 0);
+
+  //Cal_job.current_scene_=1;
+
+  EXPECT_EQ(525, intrinsics[0]);//focal_length_x from camera yaml file
+  EXPECT_EQ(525, intrinsics[1]);//focal_length_y from camera yaml file
+  EXPECT_EQ(320, intrinsics[2]);//center_x from camera yaml file
+  EXPECT_EQ(240, intrinsics[3]);//center_y from camera yaml file
+  EXPECT_EQ(0.01, intrinsics[4]);//distortion_k1 from camera yaml file
+  EXPECT_EQ(0.02, intrinsics[5]);//distortion_k2 from camera yaml file
+  EXPECT_EQ(0.03, intrinsics[6]);//distortion_k3 from camera yaml file
+  EXPECT_EQ(0.01, intrinsics[7]);//distortion_p1 from camera yaml file
+  EXPECT_EQ(0.01, intrinsics[8]);//distortion_p2 from camera yaml file
+/*  ROS_INFO_STREAM("instrinsics 0: "<<intrinsics[0]);
+  ROS_INFO_STREAM("instrinsics 1: "<<intrinsics[1]);
+  ROS_INFO_STREAM("instrinsics 2: "<<intrinsics[2]);
+  ROS_INFO_STREAM("instrinsics 3: "<<intrinsics[3]);
+  ROS_INFO_STREAM("instrinsics 4: "<<intrinsics[4]);
+  ROS_INFO_STREAM("instrinsics 5: "<<intrinsics[5]);
+  ROS_INFO_STREAM("instrinsics 6: "<<intrinsics[6]);
+  ROS_INFO_STREAM("instrinsics 7: "<<intrinsics[7]);
+  ROS_INFO_STREAM("instrinsics 8: "<<intrinsics[8]);
+  ROS_INFO_STREAM("extrinsics 0: "<<extrinsics[0]);
+  ROS_INFO_STREAM("extrinsics 1: "<<extrinsics[1]);
+  ROS_INFO_STREAM("extrinsics 2: "<<extrinsics[2]);
+  ROS_INFO_STREAM("extrinsics 3: "<<extrinsics[3]);
+  ROS_INFO_STREAM("extrinsics 4: "<<extrinsics[4]);
+  ROS_INFO_STREAM("extrinsics 5: "<<extrinsics[5]);*/
+  EXPECT_EQ(1.0, extrinsics[0]);//angle_axis_ax in camera yaml file
+  EXPECT_EQ(1.1, extrinsics[1]);//angle_axis_ay in camera yaml file
+  EXPECT_EQ(1.2, extrinsics[2]);//angle_axis_az in camera yaml file
+  EXPECT_EQ(1.3, extrinsics[3]);//position_x in camera yaml file
+  EXPECT_EQ(1.4, extrinsics[4]);//position_y in camera yaml file
+  EXPECT_EQ(1.5, extrinsics[5]);//position_z in camera yaml file
+/*  ROS_INFO_STREAM("target_pose 0: "<<target_pose[0]);
+  ROS_INFO_STREAM("target_pose 1: "<<target_pose[1]);
+  ROS_INFO_STREAM("target_pose 2: "<<target_pose[2]);
+  ROS_INFO_STREAM("target_pose 3: "<<target_pose[3]);
+  ROS_INFO_STREAM("target_pose 4: "<<target_pose[4]);
+  ROS_INFO_STREAM("target_pose 5: "<<target_pose[5]);*/
+  EXPECT_EQ(2.2, target_pose[0]);//position_x from target yaml
+  EXPECT_EQ(2.3, target_pose[1]);//position_y from target yaml
+  EXPECT_EQ(2.4, target_pose[2]);//position_z from target yaml
+  EXPECT_EQ(1.1, target_pose[3]);//angle_axis_ax from target yaml
+  EXPECT_EQ(2.0, target_pose[4]);//angle_axis_ay from target yaml
+  EXPECT_EQ(2.1, target_pose[5]);//angle_axis_ay from target yaml
+
+  //check first and last points (from target yaml)
+  EXPECT_EQ(0.0, pnt_pos[0]);
+  EXPECT_EQ(0.0, pnt_pos[1]);
+  EXPECT_EQ(0.0, pnt_pos[2]);
+  pnt_pos = c_blocks_.getStaticTargetPointParameterBlock("Checkerboard", 120);
+  EXPECT_EQ(0.4, pnt_pos[0]);
+  EXPECT_EQ(0.4, pnt_pos[1]);
+  EXPECT_EQ(0.0, pnt_pos[2]);
+
+  ASSERT_GT(point_ids.size(), 0);
+  //check first and last points (from observation.txt)
+  EXPECT_EQ(0,point_ids.at(0));
+  EXPECT_EQ(120,point_ids.at(120));
+  EXPECT_EQ(64.8908386230469,obs_x.at(0));
+  EXPECT_EQ(58.9467353820801,obs_x.at(120));
+  EXPECT_EQ(282.849487304688,obs_y.at(0));
+  EXPECT_EQ(280.590637207031,obs_y.at(120));
+}
+TEST(IndustrialExtrinsicCalSuite, ceres_check)
+{
+
+  CeresBlocks c_blocks_ = Cal_job.getCeresBlocks();
+    ceres::Problem problem;//=Cal_job.getProblem();
+  for (int i=0; i<point_ids.size(); i++)
+  {
+
+    pnt_pos = c_blocks_.getStaticTargetPointParameterBlock("Checkerboard", i);
+  // create the cost function
+  ceres::CostFunction* cost_function = TargetCameraReprjErrorNoDistortion::Create(obs_x.at(i), obs_y.at(i),
+                                                                           intrinsics[0],
+                                                                           intrinsics[1],
+                                                                           intrinsics[2],
+                                                                           intrinsics[3],
+                                                                           pnt_pos[0],
+                                                                           pnt_pos[1],
+                                                                           pnt_pos[2]);
+  problem.AddResidualBlock(cost_function, NULL , extrinsics, target_pose);
+  }
+  ceres::Solver::Options options;
+  options.linear_solver_type = ceres::DENSE_SCHUR;
+  options.minimizer_progress_to_stdout = true;
+  options.max_num_iterations = 1000;
+
+  ceres::Solver::Summary summary;
+  ceres::Solve(options, &problem, &summary);
+  std::cout << summary.BriefReport() << "\n";
+  //std::cout << summary.FullReport() << "\n";
+
+
+  printf("World to Camera\n");
+  printf("Optimized Camera\n");
+  print_AAtoH(extrinsics[0], extrinsics[1], extrinsics[2],
+               extrinsics[3], extrinsics[4], extrinsics[5]);
+
+  /*
+  //printf("%s\n", words.c_str());
+  printf("Camera to World Transform:\n");
+  //print_AATasHI(C.aa[0], C.aa[1], C.aa[2], C.pos[0], C.pos[1], C.pos[2]);
+  double R[9];
+  double aa[3];
+  aa[0] = extrinsics[0];
+  aa[1] = extrinsics[1];
+  aa[2] = extrinsics[2];
+  ceres::AngleAxisToRotationMatrix(aa, R);
+  double ix = -(extrinsics[3] * R[0] + extrinsics[4] * R[1] + extrinsics[5] * R[2]);
+  double iy = -(extrinsics[3] * R[3] + extrinsics[4] * R[4] + extrinsics[5] * R[5]);
+  double iz = -(extrinsics[3] * R[6] + extrinsics[4] * R[7] + extrinsics[5] * R[8]);
+  printf("%6.3lf %6.3lf %6.3lf %6.3lf\n", R[0], R[1], R[2], ix);
+  printf("%6.3lf %6.3lf %6.3lf %6.3lf\n", R[3], R[4], R[5], iy);
+  printf("%6.3lf %6.3lf %6.3lf %6.3lf\n", R[6], R[7], R[8], iz);
+  printf("%6.3lf %6.3lf %6.3lf %6.3lf\n", 0.0, 0.0, 0.0, 1.0);*/
+}
+// angle axis to homogeneous transform inverted
+void print_AAtoH(double x, double y, double z, double tx, double ty, double tz)
+{
+  double R[9];
+  double aa[3];
+  aa[0] = x;
+  aa[1] = y;
+  aa[2] = z;
+  ceres::AngleAxisToRotationMatrix(aa, R);
+  printf("%6.3lf %6.3lf %6.3lf %6.3lf\n", R[0], R[3], R[6], tx);
+  printf("%6.3lf %6.3lf %6.3lf %6.3lf\n", R[1], R[4], R[7], ty);
+  printf("%6.3lf %6.3lf %6.3lf %6.3lf\n", R[2], R[5], R[8], tz);
+  printf("%6.3lf %6.3lf %6.3lf %6.3lf\n", 0.0, 0.0, 0.0, 1.0);
+}
+
 
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv)
 {
+  ros::init(argc, argv, "test");
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
