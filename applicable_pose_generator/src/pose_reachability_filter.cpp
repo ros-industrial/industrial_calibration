8be106d1f1c55d8534f929c323b05a7a902ca216
Lawrence Lewis <lawrenceglewis@ttu.edu>
2018-08-01 10:07:24 -0500

    hello git commit

--- applicable_pose_generator/src/pose_reachability_filter.cpp
+++ applicable_pose_generator/src/pose_reachability_filter.cpp
@@ -1,4 +1,3 @@
-#include <tesseract_ros/kdl/kdl_chain_kin.h>
 #include <urdf_model/model.h>
 
 #include <kdl/chainiksolverpos_lma.hpp>
@@ -11,47 +10,53 @@
 #include <kdl_parser/kdl_parser.hpp>
 #include <eigen_conversions/eigen_kdl.h>
 
-namespace create_chain_take_pose_inverse_kinamatics
-{
 
-
-bool chain_creation::chain_Parse(Eigen::Affine3d ei_transform_to_check)
+namespace CreateChain
 {
-  urdf::Model Mymodel;
-  ros::NodeHandle nh("~");
-  std::string robot_urdf;
-  if (!nh.getParam("robot_urdf", robot_urdf)){
+chain_creation::chain_creation()
+{
+  ros::NodeHandle pnh("~");
+  if (!pnh.getParam("robot_urdf", robot_urdf)){
     ROS_ERROR("did not set parameter robot_urdf");
+  }
+  if (!pnh.getParam("base_link", base_link_param)){
+    ROS_ERROR("did not set parameter base_link");
+
+  }
+  if (!pnh.getParam("tool0", tool0_param)){
+    ROS_ERROR("did not set parameter tool0");
 
   }
   if (!Mymodel.initFile(robot_urdf))
   {
     ROS_ERROR("Failed to parse urdf file");
-    return false;
+    return;
   }
-  //todo link to boost and then make shared
+  else
+  {
   ROS_INFO("Successfully parsed urdf file");
+  }
   urdf::ModelInterfaceConstSharedPtr model_ptr = boost::make_shared<urdf::Model>(Mymodel);
-  tesseract::tesseract_ros::KDLChainKin CK;
-  if(!CK.init(model_ptr,"base_link","tool0","Lawrence_Robot"))
+  if(!CK.init(model_ptr,base_link_param,tool0_param,"Robot_from_urdf"))
   {
     ROS_ERROR("failed to initiat the chain");
-    return false;
+    return;
   }
-  KDL::Tree robot_tree;
   kdl_parser::treeFromUrdfModel(*model_ptr, robot_tree);
-  KDL::Chain robot_chain;
-  robot_tree.getChain("base_link","tool0",robot_chain);
-  KDL::ChainIkSolverPos_LMA solving_Ik (robot_chain);
-  KDL::JntArray robot_joints(robot_chain.getNrOfJoints()); //
-  KDL::JntArray return_joint_values(robot_chain.getNrOfJoints());
+  robot_tree.getChain(base_link_param,tool0_param,robot_chain);
+}
 
-  //get transfor that we are trying to reach with robot
-
-  KDL::Frame transform_goal_kdl;
+bool chain_creation::chain_Parse(Eigen::Affine3d ei_transform_to_check)
+{
+  //get transform that we are trying to reach with robot
+  KDL::ChainIkSolverPos_LMA solving_Ik(robot_chain);
+  robot_joints.resize(robot_chain.getNrOfJoints());
+  return_joint_values.resize(robot_chain.getNrOfJoints());
   tf::transformEigenToKDL(ei_transform_to_check,transform_goal_kdl);
+
   ROS_INFO("%d",solving_Ik.CartToJnt(robot_joints,transform_goal_kdl,return_joint_values));
-  if(!solving_Ik.CartToJnt(robot_joints,transform_goal_kdl,return_joint_values))
+
+  if(0 == solving_Ik.CartToJnt(robot_joints,transform_goal_kdl,return_joint_values))
   {
     ROS_INFO("Reachable Pose Found");
     return true;
@@ -61,7 +66,6 @@ bool chain_creation::chain_Parse(Eigen::Affine3d ei_transform_to_check)
     return false;
     ROS_INFO("Unreachable pose found");
   }
-  return false;
 }
 }//end of namespace create_chain_take_pose_inverse_kinamatics
 
