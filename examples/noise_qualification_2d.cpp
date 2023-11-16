daa20d56db2c54db56050b0e81b366951ebef68e
Michael Ripperger <michael.ripperger@swri.org>
2023-11-16 09:08:37 -0500

    Revised noise qualification example

--- examples/noise_qualification_2d.cpp
+++ examples/noise_qualification_2d.cpp
@@ -1,5 +1,5 @@
 #include <industrial_calibration/optimizations/analysis/noise_qualification.h>
-#include <industrial_calibration/target_finders/modified_circle_grid_target_finder.h>
+#include <industrial_calibration/target_finders/target_finder.h>
 #include <industrial_calibration/target_finders/utils/utils.h>
 #include <industrial_calibration/serialization.h>
 
@@ -11,6 +11,7 @@ using path = std::filesystem::path;
 using path = std::experimental::filesystem::path;
 #endif
 
+#include <boost_plugin_loader/plugin_loader.hpp>
 #include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
@@ -20,26 +21,28 @@ using namespace industrial_calibration;
 
 std::string WINDOW = "window";
 
-PnPNoiseStat run()
+PnPNoiseStat run(const std::filesystem::path& calibration_file)
 {
   // Parse parameters
-  path data_path = path(EXAMPLE_DATA_DIR) / path("noise_qualification");
+  YAML::Node config = YAML::LoadFile(calibration_file.string());
 
   // Load the target finder
-  YAML::Node target_finder_config = YAML::LoadFile((data_path / "target_finder.yaml").string());
-  ModifiedCircleGridTargetFinderFactory factory;
-  auto target_finder = factory.create(target_finder_config);
+  boost_plugin_loader::PluginLoader loader;
+  loader.search_libraries.insert(INDUSTRIAL_CALIBRATION_PLUGIN_LIBRARIES);
+  loader.search_libraries_env = INDUSTRIAL_CALIBRATION_SEARCH_LIBRARIES_ENV;
+
+  auto target_finder_config = getMember<YAML::Node>(config, "target_finder");
+  auto factory = loader.createInstance<TargetFinderFactory>(getMember<std::string>(target_finder_config, "type"));
+  auto target_finder = factory->create(target_finder_config);
 
   // Load camera intrinsics
-  YAML::Node camera_params = YAML::LoadFile((data_path / "camera_intr.yaml").string());
-  CameraIntrinsics camera = camera_params["intrinsics"].as<CameraIntrinsics>();
+  auto camera = getMember<CameraIntrinsics>(config, "intrinsics");
 
   // Load an initial guess for the camera to target transformation
-  YAML::Node pose_params = YAML::LoadFile((data_path / "camera_to_target_guess.yaml").string());
-  Eigen::Isometry3d camera_to_target_guess = pose_params["camera_to_target_guess"].as<Eigen::Isometry3d>();
+  auto camera_to_target_guess = getMember<Eigen::Isometry3d>(config, "camera_to_target_guess");
 
   // Load the data file which specifies the location of the images on which to perform the noise qualification
-  YAML::Node root = YAML::LoadFile((data_path / "data.yaml").string());
+  auto data = getMember<YAML::Node>(config, "data");
 
 #ifndef INDUSTRIAL_CALIBRATION_ENABLE_TESTING
   cv::namedWindow(WINDOW, cv::WINDOW_NORMAL);
@@ -47,12 +50,12 @@ PnPNoiseStat run()
 
   // Set up the noise qualification inputs
   std::vector<PnPProblem> problem_set;
-  problem_set.reserve(root.size());
-  for (std::size_t i = 0; i < root.size(); ++i)
+  problem_set.reserve(data.size());
+  for (std::size_t i = 0; i < data.size(); ++i)
   {
     // Each entry should have an image path. This path is relative to the root_path directory!
-    const auto img_path = root[i]["image"].as<std::string>();
-    static cv::Mat image = readImageOpenCV((data_path / img_path).string());
+    const auto img_path = data[i]["image"].as<std::string>();
+    static cv::Mat image = readImageOpenCV((calibration_file.parent_path() / img_path).string());
 
     // Find the observations in the image
     TargetFeatures target_features;
@@ -109,7 +112,8 @@ int main(int argc, char** argv)
 {
   try
   {
-    run();
+    const path calibration_file = path(EXAMPLE_DATA_DIR) / path("noise_qualification") / "cal_data.yaml";
+    run(calibration_file);
     return 0;
   }
   catch (const std::exception& ex)
@@ -125,8 +129,10 @@ int main(int argc, char** argv)
 
 TEST(NoiseQualification, NoiseQualification2D)
 {
+  const path calibration_file = path(EXAMPLE_DATA_DIR) / path("noise_qualification") / "cal_data.yaml";
+
   PnPNoiseStat stats;
-  ASSERT_NO_THROW(stats = run());
+  ASSERT_NO_THROW(stats = run(calibration_file));
 
   ASSERT_LT(stats.p_stat.stdev.norm(), 0.0005);       // meters
   ASSERT_LT(stats.q_stat.stdev, 0.1 * M_PI / 180.0);  // radians
