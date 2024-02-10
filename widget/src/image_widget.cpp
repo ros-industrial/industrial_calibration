3e3801e858c0aaa4444df85d979e81ebbf24dde2
Michael Ripperger <michael.ripperger@swri.org>
2024-02-09 19:50:43 -0600

    Added fixed aspect ratio label class

--- widget/src/image_widget.cpp
+++ widget/src/image_widget.cpp
@@ -26,3 +26,44 @@ void ImageWidget::resizeEvent(QResizeEvent *event)
   image_scaled_ = image_original_.scaled(size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
   update();  // Trigger a repaint
 }
+
+// AspectRatioPixmapLabel
+
+AspectRatioPixmapLabel::AspectRatioPixmapLabel(QWidget *parent) :
+    QLabel(parent)
+{
+    this->setMinimumSize(1,1);
+    setScaledContents(false);
+}
+
+void AspectRatioPixmapLabel::setPixmap ( const QPixmap & p)
+{
+    pix = p;
+    auto scaled_pm = scaledPixmap();
+    QLabel::setPixmap(scaled_pm);
+}
+
+int AspectRatioPixmapLabel::heightForWidth( int width ) const
+{
+    return pix.isNull() ? this->height() : ((qreal)pix.height()*width)/pix.width();
+}
+
+QSize AspectRatioPixmapLabel::sizeHint() const
+{
+    int w = this->width();
+    return QSize( w, heightForWidth(w) );
+}
+
+QPixmap AspectRatioPixmapLabel::scaledPixmap() const
+{
+    return pix.scaled(this->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
+}
+
+void AspectRatioPixmapLabel::resizeEvent(QResizeEvent * e)
+{
+    if(!pix.isNull())
+    {
+        auto scaled_pm = scaledPixmap();
+        QLabel::setPixmap(scaled_pm);
+    }
+}
