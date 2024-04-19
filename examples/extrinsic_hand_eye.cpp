0896f841cec0e80b5df0fce1001ecc64a88d59c0
Michael Ripperger <michael.ripperger@swri.org>
2024-04-19 12:00:43 -0700

    Updated examples to use updated typedefs

--- examples/extrinsic_hand_eye.cpp
+++ examples/extrinsic_hand_eye.cpp
@@ -100,8 +100,8 @@ std::tuple<ExtrinsicHandEyeResult, ExtrinsicHandEyeAnalysisStats> run(const path
   loader.search_libraries_env = INDUSTRIAL_CALIBRATION_SEARCH_LIBRARIES_ENV;
 
   YAML::Node target_finder_config = getMember<YAML::Node>(config, "target_finder");
-  auto factory = loader.createInstance<TargetFinderFactory>(getMember<std::string>(target_finder_config, "type"));
-  TargetFinder::ConstPtr target_finder = factory->create(target_finder_config);
+  auto factory = loader.createInstance<TargetFinderFactory2D3D>(getMember<std::string>(target_finder_config, "type"));
+  TargetFinder2D3D::ConstPtr target_finder = factory->create(target_finder_config);
 
   // Create a named OpenCV window for viewing the images
 #ifndef INDUSTRIAL_CALIBRATION_ENABLE_TESTING
@@ -152,7 +152,7 @@ std::tuple<ExtrinsicHandEyeResult, ExtrinsicHandEyeAnalysisStats> run(const path
 
 #ifndef INDUSTRIAL_CALIBRATION_ENABLE_TESTING
       // Show the points we detected
-      TargetFeatures target_features = target_finder->findTargetFeatures(images[i]);
+      TargetFeatures2D target_features = target_finder->findTargetFeatures(images[i]);
       cv::imshow(WINDOW, target_finder->drawTargetFeatures(images[i], target_features));
       cv::waitKey();
 #endif
