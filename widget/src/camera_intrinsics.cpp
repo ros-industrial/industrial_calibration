6a6cf750987dc7798503a4b624613018f9f3e0fe
Michael Ripperger <michael.ripperger@swri.org>
2024-04-24 09:25:16 -0500

    Updated camera intrinsics UI

--- widget/src/camera_intrinsics.cpp
+++ widget/src/camera_intrinsics.cpp
@@ -15,19 +15,19 @@ CameraIntrinsicsWidget::~CameraIntrinsicsWidget()
 
 void CameraIntrinsicsWidget::configure(const YAML::Node& node)
 {
-  ui_->fxDoubleSpinBox->setValue(node["fx"].as<double>());
-  ui_->fyDoubleSpinBox->setValue(node["fy"].as<double>());
-  ui_->cxDoubleSpinBox->setValue(node["cx"].as<double>());
-  ui_->cyDoubleSpinBox->setValue(node["cy"].as<double>());
+  ui_->double_spin_box_fx->setValue(node["fx"].as<double>());
+  ui_->double_spin_box_fy->setValue(node["fy"].as<double>());
+  ui_->double_spin_box_cx->setValue(node["cx"].as<double>());
+  ui_->double_spin_box_cy->setValue(node["cy"].as<double>());
 }
 
 YAML::Node CameraIntrinsicsWidget::save() const
 {
   YAML::Node node;
-  node["fx"] = ui_->fxDoubleSpinBox->value();
-  node["fy"] = ui_->fxDoubleSpinBox->value();
-  node["cx"] = ui_->fxDoubleSpinBox->value();
-  node["cy"] = ui_->fxDoubleSpinBox->value();
+  node["fx"] = ui_->double_spin_box_fx->value();
+  node["fy"] = ui_->double_spin_box_fy->value();
+  node["cx"] = ui_->double_spin_box_cx->value();
+  node["cy"] = ui_->double_spin_box_cy->value();
 
   return node;
 }
