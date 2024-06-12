82313a1af9dafd2d8a74c052cb3d606ca5ce3b13
Michael Ripperger <michael.ripperger@swri.org>
2024-06-12 18:03:21 -0500

    Updated GUI applications to run semi-headless when all input file names are specified

--- gui/src/app/extrinsic_hand_eye_calibration_app.cpp
+++ gui/src/app/extrinsic_hand_eye_calibration_app.cpp
@@ -1,5 +1,7 @@
 #include <industrial_calibration/gui/extrinsic_hand_eye_calibration_widget.h>
 #include <QApplication>
+#include <QMessageBox>
+#include <QTextStream>
 #include <signal.h>
 
 void handleSignal(int /*sig*/) { QApplication::instance()->quit(); }
@@ -15,11 +17,61 @@ int main(int argc, char** argv)
   industrial_calibration::ExtrinsicHandEyeCalibrationWidget w;
   w.setWindowTitle("Extrinsic Hand Eye Calibration");
   w.setWindowIcon(QIcon(":/icons/icon.jpg"));
-  w.showMaximized();
 
-  // Attempt to load configuration and observations files, if available
-  if (argc > 1) w.loadConfig(argv[1]);
-  if (argc > 2) w.loadObservations(argv[2]);
+  // Attempt to run headless if the configuration file (argv[1]), observation file (argv[2]), and results file (argv[3])
+  // are specified
+  if (argc > 3)
+  {
+    try
+    {
+      w.loadConfig(argv[1]);
+      w.loadObservations(argv[2]);
+      w.calibrate();
+      w.saveResults(argv[3]);
+      QMessageBox::StandardButton ret = QMessageBox::question(nullptr, "Calibration",
+                                                              "Successfully completed calibration and saved results. "
+                                                              "View results in the GUI?");
+      if (ret == QMessageBox::StandardButton::Yes)
+      {
+        w.showMaximized();
+        return app.exec();
+      }
+    }
+    catch (const std::exception& ex)
+    {
+      QString question;
+      QTextStream ss(&question);
+      ss << "Error: " << ex.what() << "\n\nOpen GUI to fix?";
 
-  return app.exec();
+      QMessageBox::StandardButton ret = QMessageBox::question(nullptr, "Error", question);
+      if (ret == QMessageBox::StandardButton::Yes)
+      {
+        w.show();
+        return app.exec();
+      }
+    }
+  }
+  else
+  {
+    w.showMaximized();
+
+    // Attempt to load configuration and observations files if available
+    try
+    {
+      if (argc > 1)
+      {
+        w.loadConfig(argv[1]);
+        QMessageBox::information(nullptr, "Configuration", "Successfully loaded calibration configuration");
+      }
+      if (argc > 2) w.loadObservations(argv[2]);
+    }
+    catch (const std::exception& ex)
+    {
+      QMessageBox::warning(nullptr, "Error", ex.what());
+    }
+
+    return app.exec();
+  }
+
+  return 0;
 }
