e737f36eed07115710785a41aa0ceb49f468768b
David Spielman <david.spielman7@gmail.com>
2024-12-06 15:13:49 -0600

    Added Dockerfile and docker compose files

---
+++ docker/extrinsic_calibration_gui.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /opt/industrial_calibration/install/setup.bash
+./opt/industrial_calibration/install/industrial_calibration/bin/industrial_calibration_extrinsic_hand_eye_calibration_app
