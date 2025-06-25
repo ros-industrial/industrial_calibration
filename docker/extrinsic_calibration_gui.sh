6353f0d49f3a41db700245e60f5be4832e9d76aa
Michael Ripperger <michael.ripperger@swri.org>
2025-06-25 14:47:11 -0500

    Updated Dockerfile to use tmpfs mount

--- docker/extrinsic_calibration_gui.sh
+++ docker/extrinsic_calibration_gui.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /opt/industrial_calibration/install/setup.bash
-./opt/industrial_calibration/install/industrial_calibration/bin/industrial_calibration_extrinsic_hand_eye_calibration_app
+source /opt/industrial_calibration/setup.bash
+./opt/industrial_calibration/industrial_calibration/bin/industrial_calibration_extrinsic_hand_eye_calibration_app
--- docker/intrinsic_calibration_gui.sh
+++ docker/intrinsic_calibration_gui.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /opt/industrial_calibration/install/setup.bash
-./opt/industrial_calibration/install/industrial_calibration/bin/industrial_calibration_camera_intrinsic_calibration_app
+source /opt/industrial_calibration/setup.bash
+./opt/industrial_calibration/industrial_calibration/bin/industrial_calibration_camera_intrinsic_calibration_app
