cef45d07b39c93e51240899614a59c134bbf536c
David Spielman <76972296+DavidSpielman@users.noreply.github.com>
2025-06-25 16:36:52 -0500

    Added Dockerfile and docker compose files (#206)
    * Added Dockerfile and docker compose files
    
    * Removed ROS_DISTRO argument. Added full executable name to entrypoints
    
    * Commented out Nvidia GPU settings in docker-compose.yml
    
    * Updated ROS2 install procedure based on signing key migration
    
    * Updated Dockerfile to use tmpfs mount
    
    * Added Docker CI job
    
    ---------
    
    Co-authored-by: Michael Ripperger <michael.ripperger@swri.org>

---
+++ docker/extrinsic_calibration_gui.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /opt/industrial_calibration/setup.bash
+./opt/industrial_calibration/industrial_calibration/bin/industrial_calibration_extrinsic_hand_eye_calibration_app
---
+++ docker/intrinsic_calibration_gui.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /opt/industrial_calibration/setup.bash
+./opt/industrial_calibration/industrial_calibration/bin/industrial_calibration_camera_intrinsic_calibration_app
