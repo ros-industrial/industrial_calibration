b501538905c8dff2f8a13fcaee213ef58e3041e8
Michael Ripperger <michael.ripperger@swri.org>
2025-08-20 18:23:53 -0500

    Moved homography calculation utilties from analysis module into core

--- core/include/industrial_calibration/core/utils.h
+++ core/include/industrial_calibration/core/utils.h
@@ -156,4 +156,47 @@ Pose6d poseEigenToCal(const Eigen::Isometry3d& pose);
 
 Eigen::Isometry3d poseCalToEigen(const Pose6d& pose);
 
+/*
+ * @brief Computes the homography matrix of a set of correspondences
+ * @details There is a 3x3 homography matrix, H, that can transform a point from one plane onto a different plane
+ *
+ * | u | = k * | H00 H01 H02 | * | x |
+ * | v |       | H10 H11 H12 |   | y |
+ * | 1 |       | H20 H12  1  |   | 1 |
+ *
+ * In our case we have 2 sets of known corresponding planar points: points on the planar target, and points in the
+ * image plane Therefore, there is some matrix, H, which can transform target points into the image plane. If the
+ * target points and camera points actually match, we should be able to:
+ *   1. Calculate H for a subset of corresponding points
+ *   2. Transform the remaining target points by H to obtain estimates of their locations in the image plane
+ *   3. Compare the calculated estimations to the actual image points to make sure they are very close. If they are
+ * not close, we know that the correspondences are not valid
+ *
+ * The matrix H has 8 unique values.
+ * These 8 values of the homography matrix can be solved for, given a set of (at least) 8 corresponding planar
+ * vectors, by rearranging the above equations:
+ *
+ * A * H = b, where
+ * H = inv(A) * b
+ *   - A is matrix (size 2*n x 8), where n is the number of corresponding vectors
+ *   - H is a vector (size 8 x 1) of the unknown elements of the homography matrix
+ *   - B is a vector (size 2*n x 1) representing the elements of one set of planar vectors
+ *
+ *                  A               *              H    =    b
+ * |-x0 -y0  -1   0    0    0   u0*x0   u0*y0 | * | H00 | = | -u0 |
+ * | 0   0   0  -x0   -y0  -1   v0*x0   v0*y0 | * | H01 | = | -v0 |
+ *                              ...
+ * |-x7 -y7  -1   0    0    0   u7*x7   u7*y7 | * | H20 | = | -u7 |
+ * | 0   0   0  -x7   -y7  -1   v7*x7   v7*y7 | * | H21 | = | -v7 |
+ *
+ */
+Eigen::Matrix<double, 3, 3, Eigen::RowMajor> calculateHomography(const Correspondence2D3D::Set correspondences);
+
+/**
+ * @brief Projects a set of planar 2D points in world coordinates into image coordinates using a homography matrix
+ * @param H Homography matrix
+ * @param xy 2D planar points in world coordinates
+ */
+Eigen::Matrix2Xd projectHomography(const Eigen::Matrix<double, 3, 3, Eigen::RowMajor>& H, const Eigen::Matrix2Xd& xy);
+
 }  // namespace industrial_calibration
--- core/src/utils.cpp
+++ core/src/utils.cpp
@@ -50,4 +50,55 @@ Eigen::Isometry3d poseCalToEigen(const Pose6d& pose)
   return p;
 }
 
+Eigen::Matrix<double, 3, 3, Eigen::RowMajor> calculateHomography(const Correspondence2D3D::Set correspondences)
+{
+  Eigen::MatrixXd A(2 * correspondences.size(), 8);
+  Eigen::MatrixXd b(2 * correspondences.size(), 1);
+
+  // Fill the A and B matrices with data from the selected correspondences
+  for (std::size_t i = 0; i < correspondences.size(); ++i)
+  {
+    const Correspondence2D3D& corr = correspondences[i];
+
+    // assign A row-th row:
+    const double x = corr.in_target.x();
+    const double y = corr.in_target.y();
+    const double u = corr.in_image.x();
+    const double v = corr.in_image.y();
+    A.row(2 * i) << -x, -y, -1.0, 0.0, 0.0, 0.0, u * x, u * y;
+    A.row(2 * i + 1) << 0.0, 0.0, 0.0, -x, -y, -1.0, v * x, v * y;
+
+    b.block<2, 1>(2 * i, 0) = -1.0 * corr.in_image;
+  }
+
+  // Create the homography matrix
+  Eigen::Matrix<double, 3, 3, Eigen::RowMajor> H = Eigen::Matrix3d::Ones();
+
+  // Map the elements of the H matrix into a column vector and solve for the first 8
+  {
+    Eigen::Map<Eigen::VectorXd> hv(H.data(), 9);
+    hv.head<8>() = A.fullPivLu().solve(b);
+  }
+
+  return H;
+}
+
+Eigen::Matrix2Xd projectHomography(const Eigen::Matrix<double, 3, 3, Eigen::RowMajor>& H, const Eigen::Matrix2Xd& xy)
+{
+  Eigen::Matrix3Xd pts = Eigen::Matrix3Xd::Ones(3, xy.cols());
+  pts.block(0, 0, 2, xy.cols()) = xy;
+
+  // Perform the projection
+  Eigen::Matrix3Xd uv = H * pts;
+
+  // Calculate the per-point scaling factors (shape [n, 1])
+  // k = 1.0 / (H[2, 0] * x + H[2, 1] * y + 1)
+  Eigen::ArrayXd k = (H.row(2) * pts).array().cwiseInverse();
+
+  // Apply the scale factor through elementwise multiplication
+  uv.array().rowwise() *= k.transpose();
+
+  return uv.block(0, 0, 2, uv.cols());
+}
+
 }  // namespace industrial_calibration
